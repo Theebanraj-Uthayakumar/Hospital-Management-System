6d02c8ed358298bb30e6b370e78d6e78bc162942
DougDeMuro <dougdemurocars@gmail.com>
2022-09-09 12:29:40 +0530

    base files changed

--- Client-Service/src/App.js
+++ Client-Service/src/App.js
@@ -1,12 +1,14 @@
-import logo from "./logo.svg";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h3>Welcome to React!</h3>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
