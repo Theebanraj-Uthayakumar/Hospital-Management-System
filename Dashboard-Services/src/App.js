a6c20dbef1db929cc1b55c68763ab6ff883a817d
Theeban <tuthayakumar@zapmii.com>
2022-09-09 19:40:23 +0530

    doctors part added

--- Dashboard-Services/src/App.js
+++ Dashboard-Services/src/App.js
@@ -6,6 +6,10 @@ import { MDBCard, MDBCardTitle, MDBCardText, MDBCardBody, MDBCardImage, MDBRow, 
 import Dashbord from "./components/Dashbord";
 import Operationadd from "./components/pages/Operation/Operationadd";
 import Operationupdate from "./components/pages/Operation/Operationupdate";
+import Doctors_add from "./components/pages/Doctors/Doctors_add";
+import Doctors_update from "./components/pages/Doctors/Doctors_update";
+import Doctors_Edit from "./components/pages/Doctors/Doctors_Edit"
+import Doctors_Report from "./components/pages/Doctors/Doctors_Report";
 
 // All routing routes ddefined here
 const Routing = () => {
@@ -14,6 +18,10 @@ const Routing = () => {
       <Route exact path="/" component={Dashbord} />
       <Route exact path="/Operationadd" component={Operationadd} />
       <Route exact path="/Operationupdate" component={Operationupdate} />
+      <Route exact path="/Doctors_Edit" component={Doctors_Edit} />
+      <Route exact path="/Doctors_Report" component={Doctors_Report} />
+      <Route exact path="/Doctors_update" component={Doctors_update} />
+      <Route exact path="/Doctors_add" component={Doctors_add} />
     </Switch>
   );
 };
